85594714dfcbc96cc0d7c45b21faaabc8d9a1734
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-03-04 14:56:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# GOV.UK Markdown
+
+Convert Markdown into [GOV.UK Frontend](https://github.com/alphagov/govuk-frontend)-compliant HTML. It’s an extension to the [marked](https://marked.js.org) renderer and is inspired by its Ruby equivalent gem [`govuk_markdown`](https://github.com/DFE-Digital/govuk_markdown).
+
+Don’t confuse this package with [govspeak](https://github.com/alphagov/govspeak), which is a Markdown dialect specifically built for the GOV.UK publishing system (www.gov.uk).
+
+## Requirements
+
+Node.js v16 or later.
+
+## Installation
+
+`npm install govuk-markdown --save`
+
+## Usage
+
+Import `GovukHTMLRenderer` and set it as the `renderer` in marked’s options:
+
+```js
+import { marked } from 'marked'
+import GovukHTMLRenderer from 'govuk-markdown'
+
+marked.setOptions({
+  renderer: new GovukHTMLRenderer()
+})
+```
+
+When you call `marked`, the generated HTML will include the classes from GOV.UK Frontend. For example:
+
+```js
+marked('[A link](/foo)')
+```
+
+Will output:
+
+```html
+<p class="govuk-body">
+  <a class="govuk-link" href="/foo">A link</a>
+</p>
+```
+
+### Code highlighting
+
+Block code is highlighted using [highlight.js](https://highlightjs.org). For example:
+
+```js
+marked('```js\nconsole.log(\'Hello, World!\')\n```')
+```
+
+Will output:
+
+```html
+<pre class="x-govuk-code x-govuk-code--block" tabindex="0">
+  <code class="x-govuk-code__language--js">
+    <span class="x-govuk-code__variable">console</span>.<span class="x-govuk-code__title">log</span>(<span class="x-govuk-code__string">'Hello, World!'</span>)
+  </code>
+</pre>
+```
+
+To enable the styling you will need to add the following to your Sass file:
+
+```scss
+@import "govuk-markdown/x-govuk/all";
+```
+
+## Options
+
+In addition to [marked’s options](https://marked.js.org/using_advanced#options), this extension accepts additional values:
+
+| Name | Type | Description |
+| - | - | - |
+| `headingsStartWith` | `string` | Heading size to use for the top-level heading (`xl` or `l`). Default is `l`. |
+
+For example:
+
+```js
+marked.setOptions({
+  renderer: new GovukHTMLRenderer(),
+  headerIds: false,
+  headingsStartWith: 'xl',
+  smartypants: true
+})
+
+marked('# This is an extra large heading -- smart!')
+```
+
+Will output:
+
+```html
+<h1 class="govuk-heading-xl">This is an extra large heading – smart!</h1>
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+const highlightJs = require('highlight.js')
+const { Renderer } = require('marked').marked
+
+/**
+ * Creates a new marked Renderer. Adds GOV.UK typography classes to block
+ * quotes, headings, paragraphs, links, lists, section breaks and tables and
+ * updates references to local files in links and images to friendly URLs.
+ *
+ * @class
+ */
+module.exports = class GovukHTMLRenderer extends Renderer {
+  // Block quotes
+  blockquote (quote) {
+    return `<blockquote class="govuk-inset-text govuk-!-margin-left-0">${quote}</blockquote>`
+  }
+
+  // Headings
+  heading (text, level, string, slugger) {
+    // Headings can start with either `xl` or `l` size modifier
+    const modifiers = [
+      ...(this.options.headingsStartWith === 'xl' ? ['xl'] : []),
+      'l',
+      'm'
+    ]
+    const modifier = modifiers[level - 1] || 's'
+    const id = slugger.slug(text)
+    return `<h${level} class="govuk-heading-${modifier}" id="${id}">${text}</h${level}>`
+  }
+
+  // Paragraphs
+  paragraph (string) {
+    return `<p class="govuk-body">${string}</p>`
+  }
+
+  // Links
+  link (href, title, text) {
+    return `<a class="govuk-link" href="${href}">${text}</a>`
+  }
+
+  // Lists
+  list (body, ordered) {
+    const element = ordered ? 'ol' : 'ul'
+    const modifier = ordered ? 'number' : 'bullet'
+
+    return `<${element} class="govuk-list govuk-list--${modifier}">${body}</${element}>`
+  }
+
+  // Section break
+  hr () {
+    return '<hr class="govuk-section-break govuk-section-break--xl govuk-section-break--visible">'
+  }
+
+  // Tables
+  table (header, body) {
+    return `<table class="govuk-table">
+      <thead class="govuk-table__head">${header}</thead>
+      <tbody class="govuk-table__body">${body}</tbody>
+    </table>`
+  }
+
+  tablerow (content) {
+    return `<tr class="govuk-table__row">${content}</tr>`
+  }
+
+  tablecell (content, { header, align }) {
+    const element = header ? 'th' : 'td'
+    const className = header ? 'govuk-table__header' : 'govuk-table__cell'
+    const alignClass = align ? ` govuk-!-text-align-${align}"` : ''
+    return `<${element} class="${className}${alignClass}">${content}</${element}>`
+  }
+
+  // Block code
+  // By not using marked’s `highlight` option, we can add a class to the container
+  code (string, language) {
+    highlightJs.configure({ classPrefix: 'x-govuk-code__' })
+
+    if (language) {
+      // Code language has been set, or can be determined
+      let code
+      if (highlightJs.getLanguage(language)) {
+        code = highlightJs.highlight(string, { language }).value
+      } else {
+        code = highlightJs.highlightAuto(string).value
+      }
+      return `<pre class="x-govuk-code x-govuk-code--block" tabindex="0"><code class="x-govuk-code__language--${language}">${code}</code></pre>`
+    } else {
+      // No language found, so render as plain text
+      return `<pre class="x-govuk-code x-govuk-code--block" tabindex="0">${string}</pre>`
+    }
+  }
+
+  // Inline code
+  codespan (code) {
+    return `<code class="x-govuk-code x-govuk-code--inline">${code}</code>`
+  }
+}
