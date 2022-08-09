958a8f4a42d4ecf114f2c954f6cddc5fcaea9151
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-08-09 17:27:29 +0100

    Update heading tests

--- tests/index.mjs
+++ tests/index.mjs
@@ -11,17 +11,12 @@ test('Renders blockquote', t => {
   t.is(result, '<blockquote class="govuk-inset-text govuk-!-margin-left-0"><p class="govuk-body">Blockquote</p>\n</blockquote>\n')
 })
 
-test('Renders heading', t => {
-  const result = marked('# Heading')
-  t.is(result, '<h1 class="govuk-heading-l" id="heading">Heading</h1>')
-})
-
-test('Renders headings using `s` size for lower heading levels', t => {
+test('Renders headings', t => {
   const result = marked('# Heading 1\n## Heading 2\n### Heading 3\n#### Heading 4')
   t.is(result, '<h1 class="govuk-heading-l" id="heading-1">Heading 1</h1><h2 class="govuk-heading-m" id="heading-2">Heading 2</h2><h3 class="govuk-heading-s" id="heading-3">Heading 3</h3><h4 class="govuk-heading-s" id="heading-4">Heading 4</h4>')
 })
 
-test('Renders heading classes relative to starting level', t => {
+test('Renders headings, using classes relative to given starting level', t => {
   marked.setOptions({
     headingsStartWith: 'xl'
   })
